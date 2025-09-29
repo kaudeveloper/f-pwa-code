347c490e49e39b8a97ddd886b9a65633fd646b9e
ak_pc_karaken <kaudeveloper@gmail.com>
2025-09-29 23:56:34 +0300

    +

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -53,7 +53,7 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(CORE);
+      return cache.addAll(RESOURCES);
     })
   );
 });
