3a1ed1248a5432b81f1985e47c4f4cdcf26413b5
ak_pc_karaken <kaudeveloper@gmail.com>
2025-09-30 18:34:20 +0300

    std+

--- README.md
+++
@@ -1,1 +0,0 @@
-Flutter PWA with full offline service worker
--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "305136144"
+    serviceWorkerVersion: "2644857937"
   }
 });
